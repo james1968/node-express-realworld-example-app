e0763cb4f8c02c3fb01dcd7c774dd5273dad238f
James <bakerje@mac.com>
2017-09-26 09:26:15 +0100

    Restarted User model

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
-	username: String,
-	email: String,
+	username: {type: String, lowercase: true, required: [true, "can't be blank"], match:[/^[a-zA-Z0-9]+$/, "is invalid"],index: true},
+	email: {type: String, lowercase: true, required: [true, "can't be blank"], match:[/^[a-zA-Z0-9]+$/, "is invalid"],index: true}
 	bio:String,
 	image: String,
 	hash: String,
