6d6859d251fe52baf34a0a2b841cebcd94afee4b
James <bakerje@mac.com>
2017-10-03 15:56:13 +0100

    Updated user model

--- models/User.js
+++ models/User.js
@@ -1,12 +1,14 @@
 var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
-	username: {type: String, lowercase: true, required: [true, "can't be blank"], match:[/^[a-zA-Z0-9]+$/, "is invalid"],index: true},
-	email: {type: String, lowercase: true, required: [true, "can't be blank"], match:[/^[a-zA-Z0-9]+$/, "is invalid"],index: true}
+	username: {type: String, lowercase: true, unique: true,required: [true, "can't be blank"], match:[/^[a-zA-Z0-9]+$/, "is invalid"],index: true},
+	email: {type: String, lowercase: true, unique: true,required: [true, "can't be blank"], match:[/^[a-zA-Z0-9]+$/, "is invalid"],index: true}
 	bio:String,
 	image: String,
 	hash: String,
 	salt: String
 },{timestamps: true});
+
+UserSchema.plugin(uniqueValidator,{message:'is already taken.'});
 
 mongoose.model('User', UserSchema);
