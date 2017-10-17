88ef91e72a1cd25430b7c2e2bd08edbe9f3e5c8c
James <bakerje@mac.com>
2017-10-17 16:08:27 +0100

    User model

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ if(isProduction){
   mongoose.connect('mongodb://localhost/conduit');
   mongoose.set('debug', true);
 }
-
+require('./models/User');
+require('./config/passport');
 app.use(require('./routes'));
 
 /// catch 404 and forward to error handler
